e6966095edc0c18ee756c4f7050064a42fcfef24
tembz <ap11dlayt@gmail.com>
2026-02-21 14:16:05 +0300

    убрал дубль функцию

--- handlers/credit.py
+++ handlers/credit.py
@@ -34,25 +34,6 @@ async def toggle_credit_status(credit_id: int):
     await update_credit_status(credit_id)
     return {"status": "success", "data": {"ok": True}}
 
-
-#@credit_handler.post("/create")
-async def create_new_credit(request: Request):
-    source = await request.json()
-    data = {
-        "date": datetime.today(),
-        "restaurant": int(source.get("restaurant", 0)),
-        "what_take": source.get("what_take", ""),
-        "measurement_unit": source.get("measurement_unit", ""),
-        "repayment_date": source.get("repayment_date", datetime.today()),
-        "is_transfer": bool(source.get("is_transfer", False)),
-        "credit_type": source.get("credit_type", ""),
-        "count": float(source.get("count", 0))
-    }
-    await send_credit_message_to_chat(data)
-    await add_new_credit(data)
-    return {"status": "success", "data": {"ok": True}}
-
-
 @credit_handler.post("/create")
 async def create_new_credit(request: Request):
     form = await request.form()
